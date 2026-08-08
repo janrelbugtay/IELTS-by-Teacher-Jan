d2bcb987487a51e9aeb764661c5d1ce77c11ed31
janrelbugtay <janrelbugtay03@gmail.com>
2026-08-08 10:29:04 +0700

    refactor: remove legacy test parts and scripts
    Removed hardcoded assignment parts and cleanup scripts to declutter the codebase as these are no longer required for the current test implementation.

--- strip_part4.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-let content = fs.readFileSync('src/pages/OctoberListeningTest.tsx', 'utf8');
-
-const startStr = "currentPartIndex === 4";
-
-const startIndex = content.indexOf(startStr);
-console.log(startIndex);
